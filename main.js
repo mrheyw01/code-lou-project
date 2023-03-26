3a370c113e74af8708bf4e87d812fb0b7fc806cd
Madison Heyward <madisonheyward@rocketmail.com>
2023-03-26 15:14:50 -0400

    Changes from 3/26

--- main.js
+++ main.js
@@ -1,3 +1,38 @@
+const toggle = document.querySelector(".toggle");
+const menu = document.querySelector(".menu");
+/* TOGGLE MOBILE MENU */
+function toggleMenu() {
+    if (menu.classList.contains("active")) {
+        menu.classList.remove("active");
+        
+        // THIS ADDS THE HAMBURGER MENU ICON 
+        toggle.querySelector("a").innerHTML = "<i class='fas fa-bars'></i>";
+    } else {
+        menu.classList.add("active");
+        
+        // THIS ADDS THE CLOSE ICON ONCE HAMBURGER MENU IS OPEN 
+        toggle.querySelector("a").innerHTML = "<i class='fas fa-times'></i>";
+    }
+}
+/* EVENT LISTENER */
+toggle.addEventListener("click", toggleMenu, false);
+
+
+/* CLOSE SUBMENU FROM ANYWHERE */
+function closeSubmenu(e) {
+  if (menu.querySelector(".submenu-active")) {
+    let isClickInside = menu
+      .querySelector(".submenu-active")
+      .contains(e.target);
+    if (!isClickInside && menu.querySelector(".submenu-active")) {
+      menu.querySelector(".submenu-active").classList.remove("submenu-active");
+    }
+  }
+}
+/* EVENT LISTENER */
+document.addEventListener("click", closeSubmenu, false);
+
+
 var acc = document.getElementsByClassName("accordion");
 var i;
 
